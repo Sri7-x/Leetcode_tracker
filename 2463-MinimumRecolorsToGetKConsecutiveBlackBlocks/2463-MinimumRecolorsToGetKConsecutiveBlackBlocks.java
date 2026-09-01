47b84845b7ec951618f7ac389b908149049354b6
Sri7-x <sriram20082005@gmail.com>
2026-09-01 12:41:13 +0530

    Create 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks

---
+++ 2463-MinimumRecolorsToGetKConsecutiveBlackBlocks/2463-MinimumRecolorsToGetKConsecutiveBlackBlocks.java
@@ -0,0 +1,21 @@
+// Last updated: 1/9/2026, 12:41:12 pm
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int  min=k,count=0,n=blocks.length();
+        for(int i=0;i<k;i++){
+            if(blocks.charAt(i)=='W'){
+                count++;
+            }
+        }min =count;
+        for(int i=1;i<n-k+1;i++){
+            if(blocks.charAt(i-1)=='W'){
+                count--;
+
+            }if(blocks.charAt(i+k-1)=='W'){
+                count++;
+            }if(count<min){
+                min=count;
+            }
+        }return  min;        
+    }
+}
