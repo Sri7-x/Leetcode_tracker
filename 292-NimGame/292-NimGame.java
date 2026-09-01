1704f4d45ab7e844e600e01b289ccb5646457a6f
Sri7-x <sriram20082005@gmail.com>
2026-09-01 12:42:13 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 1/9/2026, 12:42:11 pm
+class Solution {
+    public boolean canWinNim(int n) {
+                if(n%4==0){
+                    return false;
+                }else{
+                    return true;
+                }
+    
+    }
+}
