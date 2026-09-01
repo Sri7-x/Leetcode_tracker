03fd11e54ce27348f5b6da1bee9d1e4209457489
Sri7-x <sriram20082005@gmail.com>
2026-09-01 12:41:44 +0530

    Create 775-NAryTreePreorderTraversal

---
+++ 775-NAryTreePreorderTraversal/775-NAryTreePreorderTraversal.java
@@ -0,0 +1,33 @@
+// Last updated: 1/9/2026, 12:41:42 pm
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution{
+    List<Integer> log=new LinkedList<>();
+
+    public List<Integer> preorder(Node root) {
+        if(root!=null){
+            log.add(root.val);
+            for(int i=0;i<root.children.size();i++){
+                preorder(root.children.get(i));
+            }
+        }
+        return log;
+    }
+}
