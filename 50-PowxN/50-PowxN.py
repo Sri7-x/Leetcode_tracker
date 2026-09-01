a4903bace7a6b69f465aebc5aeed8484f82b265f
Sri7-x <sriram20082005@gmail.com>
2026-09-01 12:43:17 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,10 @@
+# Last updated: 1/9/2026, 12:43:16 pm
+class Solution(object):
+    def myPow(self, x, n):
+        return x**n;
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
